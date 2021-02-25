ad20c14be2451162cc3b39b86c6dbaf1737ee3d8
143-789 <nsubhasish143@gmail.com>
2021-02-25 23:31:16 +0530

    updated startups flask model

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float
 import os
 from flask_marshmallow import Marshmallow
-'''
+
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+'''
 from flask_mail import Mail, Message
 '''
 
@@ -13,9 +14,9 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'startup_details.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-'''
-app.config['JWT_SECRET_KEY'] = 'super-secret'  # change this IRL
 
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # change this IRL
+'''
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
@@ -23,8 +24,9 @@ app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-'''
+
 jwt = JWTManager(app)
+'''
 mail = Mail(app)
 '''
 
@@ -42,30 +44,40 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    t_start1 = iDetails(strtup_name='human',
-                      foundr_name='tech',
+    t_start1 = sDetails(strtup_name='Aria Travel',
+                      foundr_name='Aashay',
+                      cofoundr_name='Aakarsh',
                       amnt_raised=12365.12,
+                      amnt_needed=250000.54,
                       no_of_members=3,
                       eqty_prop=234.23,
-                      interested_investing='something',
-                      role='founder',
+                      dt_establish=20092020,
+                      approved=True,
+                      role='founder','''
                       strtup_logo='image',
-                      invested_companies='tech',
+                      business_model= 'travel is life',
+                      business_desc= 'travel made easy',
+                      '''invested_companies='tech',
                       list_of_companies='tech1',
                       # kyc upload files
                       Adhar_card_front='adharfront',
                       Adhar_card_back='adharback',
                       pan='pan card',
                       bank_Statement='bank statement'
+                      
+                      inv_note= 'the next big thing',
+                      balanc_sheet= 'upload file',
+                      fin_details= 'upload file',
+                      inv_pitch= 'upload file'
                       )
 
     db.session.add(t_start1)
 
-    test_user = User(founder_first_name = 'manoj',
-                     founder_last_name = 'bhaiya',
+    test_user = User(founder_first_name = 'Aashay',
+                     founder_last_name = 'Kapoor',
                      phone = 23984738,
-                     email = 'email@email.com',
-                     password = 'ashishj')
+                     email = 'appdeveloper@email.com',
+                     password = 'aashaykap')
 
     db.session.add(test_user)
     db.session.commit()
@@ -75,12 +87,12 @@ def db_seed():
 
 @app.route('/')
 def hello_world():
-    return 'Hello Startups'
+    return 'Hello World!'
 
 
 @app.route('/super_simple')
 def super_simple():
-    return jsonify(message='Hello from the Startup API.'), 200
+    return jsonify(message='Hello from the Investry API.'), 200
 
 
 @app.route('/not_found')
@@ -98,8 +110,8 @@ def url_variables(name: str, age: int):
 
 @app.route('/investors_details', methods=['GET'])
 def investors_details():
-    inv_list = iDetails.query.all()
-    result = invs_schema.dump(inv_list)
+    inv_list = sDetails.query.all()
+    result = starts_schema.dump(inv_list)
     return jsonify(result.data)
 
 
@@ -163,22 +175,22 @@ def retrieve_password(email: str):
 
 @app.route('/Inv_details/<int:strtup_id>', methods=["GET"])
 def inv_details(strtup_id: int):
-    investor_dtl = iDetails.query.filter_by(strtup_id=strtup_id).first()
-    if investor_dtl:
-        result = inv_schema.dump(investor_dtl)
+    startup_dtl = sDetails.query.filter_by(strtup_id=strtup_id).first()
+    if startup_dtl:
+        result = start_schema.dump(startup_dtl)
         return jsonify(result.data)
     else:
-        return jsonify(message="That investor_dtl does not exist"), 404
+        return jsonify(message="That startup_dtl does not exist"), 404
 '''
 #create
 
-@app.route('/add_investor', methods=['POST'])
+@app.route('/add_startup', methods=['POST'])
 @jwt_required
-def add_investor():
+def add_startup():
     strtup_name = request.form['strtup_name']
-    test = iDetails.query.filter_by(strtup_name=strtup_name).first()
+    test = sDetails.query.filter_by(strtup_name=strtup_name).first()
     if test:
-        return jsonify("There is already a investor_dtl by that name"), 409
+        return jsonify("There is already a startup_dtl by that name"), 409
     else:
         foundr_name = request.form['foundr_name']
         strtup_name = request.form['strtup_name']
@@ -187,7 +199,6 @@ def add_investor():
         amnt_raised = float(request.form['amnt_raised'])
         no_of_members = int(request.form['no_of_members'])
         # eqty_prop=float['eqty_prop']
-        # interested_investing = request.form['interested_investing']
         # upload
         strtup_logo = request.form['strtup_logo']
         # invested_companies = request.form['invested_companies']
@@ -198,14 +209,13 @@ def add_investor():
         pan = request.form['pan']
         bank_statement = request.form['bank_Statement']
 
-        new_inv = iDetails(strtup_name=strtup_name,
+        new_start = sDetails(strtup_name=strtup_name,
                            foundr_name=foundr_name,
                            role=role,
                            business_model=business_model,
                            amnt_raised=amnt_raised,
                            # no_of_members=no_of_members,
                            # eqty_prop=eqty_prop,
-                           interested_investing='tech and healthcare',
                            strtup_logo=strtup_logo,
                            # table of companies investes
                            # invested_companies = invested_companies,
@@ -218,44 +228,41 @@ def add_investor():
 
                            )
 
-        db.session.add(new_inv)
+        db.session.add(new_start)
         db.session.commit()
-        return jsonify(message="You added a investor_dtl"), 201
+        return jsonify(message="You added a startup_dtl"), 201
 '''
 #update
 '''
-@app.route('/update_investors', methods=['PUT'])
+@app.route('/update_startups', methods=['PUT'])
 @jwt_required
-def update_investors():
+def update_startups():
     strtup_id = int(request.form['strtup_id'])
-    investor_dtl = iDetails.query.filter_by(strtup_id=strtup_id).first()
-    if investor_dtl:
-        investor_dtl.strtup_name = request.form['strtup_name']
-        investor_dtl.foundr_name = request.form['foundr_name']
-        investor_dtl.role = request.form['role']
-        investor_dtl.business_model = float(request.form['business_model'])
-        investor_dtl.radius = float(request.form['radius'])
-        investor_dtl.distance = float(request.form['distance'])
-        investor_dtl.amnt_raised = float(request.form['amnt_raised'])
-        investor_dtl.strtup_id = int(request.form['no_of_members '])
-        investor_dtl.business_model = float['eqty_prop = Column(Float)']
-        investor_dtl.interested_investing = request.form['interested_investing']
-        investor_dtl.role = request.form['role']
+    startup_dtl = sDetails.query.filter_by(strtup_id=strtup_id).first()
+    if startup_dtl:
+        startup_dtl.strtup_name = request.form['strtup_name']
+        startup_dtl.foundr_name = request.form['foundr_name']
+        startup_dtl.role = request.form['role']
+        startup_dtl.business_model = float(request.form['business_model'])
+        startup_dtl.amnt_raised = float(request.form['amnt_raised'])
+        startup_dtl.strtup_id = int(request.form['no_of_members '])
+        startup_dtl.business_model = float['eqty_prop = Column(Float)']
+        startup_dtl.role = request.form['role']
         # upload
-        investor_dtl.strtup_logo = request.form['strtup_logo']
-        investor_dtl.business_model = request.form['business_model']
-        investor_dtl.invested_companies = request.form['invested_companies']
-        investor_dtl.list_of_companies = request.form['list_of_companies']
+        startup_dtl.strtup_logo = request.form['strtup_logo']
+        startup_dtl.business_model = request.form['business_model']
+        startup_dtl.invested_companies = request.form['invested_companies']
+        startup_dtl.list_of_companies = request.form['list_of_companies']
         # kyc upload files
-        investor_dtl.Adhar_card_front = request.form['Adhar_card_front']
-        investor_dtl.Adhar_card_back = request.form['Adhar_card_back']
-        investor_dtl.pan = request.form['pan']
-        investor_dtl.bank_Statement = request.form['bank_Statement']
+        startup_dtl.Adhar_card_front = request.form['Adhar_card_front']
+        startup_dtl.Adhar_card_back = request.form['Adhar_card_back']
+        startup_dtl.pan = request.form['pan']
+        startup_dtl.bank_Statement = request.form['bank_Statement']
 
         db.session.commit()
-        return jsonify(message="You updated a investor_dtl"), 202
+        return jsonify(message="You updated a startup_dtl"), 202
     else:
-        return jsonify(message="That investor_dtl does not exist"), 404
+        return jsonify(message="That startup_dtl does not exist"), 404
 '''
 
 '''
@@ -264,13 +271,13 @@ def update_investors():
 @app.route('/remove_investors_details/<int:strtup_id>', methods=['DELETE'])
 @jwt_required
 def remove_idetails(strtup_id: int):
-    investor_dtl = iDetails.query.filter_by(strtup_id=strtup_id).first()
-    if investor_dtl:
-        db.session.delete(investor_dtl)
+    startup_dtl = sDetails.query.filter_by(strtup_id=strtup_id).first()
+    if startup_dtl:
+        db.session.delete(startup_dtl)
         db.session.commit()
-        return jsonify(message="You deleted a investor_dtl"), 202
+        return jsonify(message="You deleted a startup_dtl"), 202
     else:
-        return jsonify(message="That investor_dtl does not exist"), 404
+        return jsonify(message="That startup_dtl does not exist"), 404
 
 '''
 
@@ -285,7 +292,7 @@ class User(db.Model):
     password = Column(String)
 
 
-class iDetails(db.Model):
+class sDetails(db.Model):
     __tablename__ = 'investor_details'
     strtup_id = Column(Integer, primary_key=True)
     strtup_name = Column(String)
@@ -297,20 +304,20 @@ class iDetails(db.Model):
     eqty_prop = Column(Float)
     dt_establish = Column(Float)
     approved = Column(Boolean, default=False)
-    '''interested_investing = Column(String)
-    role = Column(String)'''
+
+    role = Column(String)
     #file upload
     strtup_logo = Column(String)
     business_model = Column(String)
     business_desc = Column(String)
     inv_note = Column(String)
-    '''invested_companies = Column(String)
+    invested_companies = Column(String)
     list_of_companies = Column(String)
     # kyc upload files
     Adhar_card_front = Column(String)
     Adhar_card_back = Column(String)
     pan = Column(String)
-    bank_Statement = Column(String)'''
+    bank_Statement = Column(String)
     balnc_sheet = Column(String)
     fin_details = Column(String)
     inv_pitch = Column(String)
@@ -321,26 +328,27 @@ class UserSchema(ma.Schema):
     class Meta:
         fields = ('id', 'founder_first_name', 'founder_last_name', 'phone', 'email', 'password')
 
-
-class InvSchema(ma.Schema):
+'''
+class StartSchema(ma.Schema):
     class Meta:
         fields = ('strtup_id', 'strtup_name', 'foundr_name', 'cofoundr_name', 'amnt_raised', 'amnt_needed', 'no_of_members', 'eqty_prop', 'dt_establish', 'strtup_logo', 'approved',
         'business_model', 'business_desc', 'inv_note', 'balnc_sheet', 'fin_details', 'inv_pitch'
                   )
 '''
-class InvSchema(ma.Schema):
+class StartSchema(ma.Schema):
     class Meta:
-        fields = ('strtup_id', 'strtup_name', 'foundr_name', 'amnt_raised', 'amnt_needed', 'no_of_members', 'eqty_prop', 'interested_investing',
+        fields = ('strtup_id', 'strtup_name', 'foundr_name', 'amnt_raised', 'amnt_needed', 'no_of_members', 'eqty_prop', 'deleteline',
                    'role', 'strtup_logo', 'business_model', 'invested_companies', 'list_of_companies', 'Adhar_card_front',
                   'Adhar_card_back',
                   'pan', 'bank_Statement'
                   )
-'''
+
+
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-inv_schema = InvSchema()
-invs_schema = InvSchema(many=True)
+start_schema = StartSchema()
+starts_schema = StartSchema(many=True)
 
 
 if __name__ == '__main__':
